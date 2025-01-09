49097569eff6b4cf5dc5f02856d1c5cc3e25199d
lenmorelms <72908654+lenmorereal@users.noreply.github.com>
2025-01-09 09:20:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React, { useState } from 'react';
-import ExpenseForm from '@components/ExpenseForm';
+import ExpenseForm from './components/ExpenseForm';
 import './App.css';
 
 function App() {
